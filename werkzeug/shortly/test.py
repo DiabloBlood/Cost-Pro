412f6b31a3a3880f3e4ab89f8711f14290f0fc89
DiabloBlood <huangsichao2015@gmail.com>
2018-05-21 18:24:00 -0700

    Learn Request class of werkzeug

--- werkzeug/shortly/test.py
+++ werkzeug/shortly/test.py
@@ -2,15 +2,27 @@ from werkzeug.wrappers import Request, Response
 from werkzeug.serving import run_simple
 import shortly
 
+from pprint import pprint
 
 
-@Request.application
-def application(request):
-    return Response('Hello World!')
+
+# @Request.application
+def application(environ, start_response):
+    pprint(environ)
+    request = Request(environ)
+    print request.form
+    print type(request.form)
+    print request.args
+    response = Response('hahahah')
+    return response(environ, start_response)
 
 
-import os
-print os.path.join(os.path.dirname(__file__), 'static')
 
-# if __name__ == '__main__':
-    # run_simple('10.0.2.15', 8001, application)
+
+
+
+# import os
+# print os.path.join(os.path.dirname(__file__), 'static')
+
+if __name__ == '__main__':
+    run_simple('10.0.2.15', 8001, application)
