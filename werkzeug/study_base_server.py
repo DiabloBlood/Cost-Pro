4de1bf20b1c7ae223c233cd0e4b33e8331581275
DiabloBlood <huangsichao2015@gmail.com>
2018-05-23 18:03:47 -0700

    Modified: [study_base_server]

--- werkzeug/study_base_server.py
+++ werkzeug/study_base_server.py
@@ -2,10 +2,24 @@ import logging
 import sys
 import SocketServer
 
-# logging.basicConfig(format='%(name)s: %(message)s', level=logging.DEBUG)
+
+
+logging.basicConfig(format='%(name)s: %(message)s', level=logging.DEBUG)
+
+
+class EchoRequestHandler(SocketServer.BaseRequestHandler):
+
+    def __init__(self, request, client_address, server)：
+        self.logger = logging.getLogger('EchoRequestHandler')
+        self.logger.debug('__init__')
+        SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
+        return
+
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='example.log', level=logging.DEBUG, format='%(name)s: %(message)s')
-    logging.debug('This message should go to the log file')
-    logging.info('So should this')
-    logging.warning('And this, too')
+    logger = logging.getLogger('haha')
+    print logger
+    logger2 = logging.getLogger('haha')
+    print logger2
+    logger3 = logging.getLogger('haha2')
+    print logger3
