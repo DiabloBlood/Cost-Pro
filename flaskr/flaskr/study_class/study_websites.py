e23ba3d9392b25909af545ad071e5230bf44b72c
DiabloBlood <huangsichao2015@gmail.com>
2018-09-20 00:06:47 -0700

    Change blog name

---
+++ flaskr/flaskr/study_class/study_websites.py
@@ -0,0 +1,70 @@
+# 1. metaclasses
+# https://stackoverflow.com/questions/100003/what-are-metaclasses-in-python
+
+# 2. bound and unbound methods
+# https://stackoverflow.com/questions/114214/class-method-differences-in-python-bound-unbound-and-static
+
+# 3. type vs object
+# https://www.cnblogs.com/busui/p/7283137.html
+
+'''
+https://blog.csdn.net/linuxheik/article/details/52074282
+https://codesachin.wordpress.com/2016/06/09/the-magic-behind-attribute-access-in-python/
+http://blog.konghy.cn/2016/07/04/python-attr/
+http://pyzh.readthedocs.io/en/latest/Descriptor-HOW-TO-Guide.html
+https://www.blog.pythonlibrary.org/2016/06/10/python-201-what-are-descriptors/
+https://segmentfault.com/a/1190000013425128
+http://zhaochj.github.io/2016/05/10/2016-05-10-property%E7%9A%84%E5%AE%9E%E7%8E%B0/
+
+https://stackoverflow.com/questions/22108488/are-list-comprehensions-and-functional-functions-faster-than-for-loops
+https://stackoverflow.com/questions/8097408/why-python-is-so-slow-for-a-simple-for-loop
+http://leadsift.com/loop-map-list-comprehension/
+
+https://blog.ionelmc.ro/2015/02/09/understanding-python-metaclasses/
+https://medium.com/stepping-through-the-cpython-interpreter/how-does-attribute-access-work-d19371898fee
+
+https://makina-corpus.com/blog/metier/2014/python-tutorial-understanding-python-mro-class-search-path
+https://www.python.org/download/releases/2.3/mro/
+
+### Descriptor, property, function, staticmethod, classmethod, __getattribute__
+https://docs.python.org/2/howto/descriptor.html#properties
+https://docs.python.org/3/howto/descriptor.html
+
+https://docs.python.org/3/reference/datamodel.html#descriptors
+
+### How to use python3 start projects
+http://www.patricksoftwareblog.com/steps-for-starting-a-new-flask-project-using-python3/
+
+
+### super, bound and unbound method
+https://blog.csdn.net/qq_17550379/article/details/79508630
+
+### MetaClass
+https://www.python-course.eu/python3_road_to_metaclasses.php
+https://www.python-course.eu/python3_metaclasses.php
+https://www.pythoncentral.io/how-metaclasses-work-technically-in-python-2-and-3/
+https://python-3-patterns-idioms-test.readthedocs.io/en/latest/Metaprogramming.html
+
+### super method
+https://www.python.org/download/releases/2.2.3/descrintro/#cooperation
+
+https://zhuanlan.zhihu.com/p/27310028
+
+
+
+### study werkzeug
+https://www.zoulei.net/2016/07/25/werkzeug_note_requests_response_auto_realod/
+https://www.jianshu.com/p/d4097f7550e1
+https://github.com/python/cpython/blob/master/Lib/wsgiref/simple_server.py
+https://zhuanlan.zhihu.com/p/22536382
+https://www.4async.com/2016/02/simple-implement-asyncio-to-understand-how-async-works/
+https://stackoverflow.com/questions/12625224/can-someone-give-me-an-example-of-how-select-is-alerted-to-an-fd-becoming-rea
+https://www.ibm.com/developerworks/aix/library/au-tcpsystemcalls/index.html
+
+https://gist.github.com/IamAdiSri/a379c36b70044725a85a1216e7ee9a46
+https://github.com/iamxpy/SimpleDB
+
+
+### sublime3 4 indents python, 2 indents js and html
+http://mathalope.co.uk/2017/05/19/sublime-text-3-how-to-set-different-tab-size-based-on-file-type/
+'''
