2776aaabe90c04de531f3a7d5dd801aa75bf2366
DiabloBlood <huangsichao2015@gmail.com>
2018-09-28 18:17:50 -0700

    Webpack demo

--- blog/client/README.md
+++ blog/client/README.md
@@ -1,7 +1,7 @@
 ### install webpack
 
 npm init
-npm install webpack --save-dev
+npm install webpack webpack-cli --save-dev
 
 ### install babel-core, babel-loader
 npm install babel-loader babel-core babel-preset-es2015 --save-dev
---
+++ blog/client/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: __dirname + '/src/app.js',
+    output: {
+        path: __dirname + '/bin',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+}
