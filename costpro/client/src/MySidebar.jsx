0123c3f0043c9ba17e7370ff5a739081c8c281b8
DiabloBlood <huangsichao2015@gmail.com>
2018-10-29 18:14:30 -0700

    Working on react router

---
+++ costpro/client/src/MySidebar.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
+
+//import Sidebar from 'src/components/Sidebar.jsx';
+import image from 'src/assets/img/sidebar-3.jpg';
+import logo from 'src/assets/img/reactlogo.png';
+import withStyles from "@material-ui/core/styles/withStyles";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import Drawer from "@material-ui/core/Drawer";
+
+import DashboardIcon from "@material-ui/icons/DashboardRounded";
+import HomeIcon from "@material-ui/icons/HomeRounded";
+
+import sidebarStyle from "src/assets/jss/styles/components/sidebarStyle.jsx";
+
+
+
+class MySidebar extends React.Component {
+
+  constructor(props) {
+    console.log(props)
+
+    super(props);
+  }
+
+  render() {
+    const { classes, image, logo, logoText, route } = this.props;
+
+
+    const brand = (
+      <div className={classes.logo}>
+        <a href="https://www.google.com" className={classes.logoLink}>
+          <div className={classes.logoImage}>
+            <img src={logo} alt="logo" className={classes.img} />
+          </div>
+          {logoText}
+        </a>
+      </div>
+    );
+
+    return (
+      <Drawer
+        anchor="left"
+        variant="permanent"
+        open
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        {brand}
+        <List className={classes.list}>
+          <NavLink
+            to={route.path}
+            className={classes.item}
+            activeClassName="active"
+          >
+            <ListItem button className={classes.itemLink}>
+              <ListItemIcon className={classes.itemIcon}>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary={route.sidebarName}
+                className={classes.itemText}
+              />
+            </ListItem>
+          </NavLink>
+        </List>
+
+        <div
+          className={classes.background}
+          style={{ backgroundImage: `url(${image})` }}
+        />
+      </Drawer>
+    )
+  }
+}
+
+export default withStyles(sidebarStyle)(MySidebar)
--- costpro/client/src/test.jsx
+++ costpro/client/src/test.jsx
@@ -3,42 +3,65 @@ import ReactDOM from 'react-dom';
 
 import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
-//import Sidebar from 'src/components/Sidebar.jsx';
+import MySidebar from "src/MySidebar.jsx";
+
+
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import HomeIcon from "@material-ui/icons/Home";
 import image from 'src/assets/img/sidebar-3.jpg';
 import logo from 'src/assets/img/reactlogo.png';
-import withStyles from "@material-ui/core/styles/withStyles";
-
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-
-import Drawer from "@material-ui/core/Drawer";
-
-import DashboardIcon from "@material-ui/icons/DashboardRounded";
-import HomeIcon from "@material-ui/icons/HomeRounded";
-
-import sidebarStyle from "src/assets/jss/styles/components/sidebarStyle.jsx";
 
 
+class Dashboard extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <h1>Flask + Webpack + Babel + React</h1>
+    )
+  }
+}
+
+class Test extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <h1>Testing</h1>
+    )
+  }
+}
+
+
+//dashboard will show all conclusive data
 const dashboardRoutes = [
   {
-    path: '/',
+    path: '/dashboard',
     siderbarName: 'Home',
-    icon: HomeIcon
+    icon: DashboardIcon,
+    component: Dashboard
   },
   {
-    path: '/dashboard',
-    sidebarName: 'Dashboard',
-    icon: DashboardIcon
+    path: '/table',
+    sidebarName: 'Table',
+    icon: DashboardIcon,
+    //component: 
+  },
+  {
+    path: '/test',
+    sidebarName: 'TEST'
+    icon: HomeIcon, 
+    component: Test
   }
 ]
 
-const route = {
-  path: '/',
-  sidebarName: 'Home',
-  icon: HomeIcon
-}
+
 
 class App extends React.Component {
 
@@ -49,67 +72,27 @@ class App extends React.Component {
   render() {
     const { classes, image, logo, logoText, route } = this.props;
 
-    const brand = (
-      <div className={classes.logo}>
-        <a href="https://www.google.com" className={classes.logoLink}>
-          <div className={classes.logoImage}>
-            <img src={logo} alt="logo" className={classes.img} />
-          </div>
-          {logoText}
-        </a>
-      </div>
-    );
-
     return (
-      <BrowserRouter>
-        <Drawer
-          anchor="left"
-          variant="permanent"
-          open
-          classes={{
-            paper: classes.drawerPaper
-          }}
-        >
-          {brand}
-          <List className={classes.list}>
-            <NavLink
-              to={route.path}
-              className={classes.item}
-              activeClassName="active"
-            >
-              <ListItem button className={classes.itemLink}>
-                <ListItemIcon className={classes.itemIcon}>
-                  <DashboardIcon />
-                </ListItemIcon>
-                <ListItemText
-                  primary={route.sidebarName}
-                  className={classes.itemText}
-                />
-              </ListItem>
-            </NavLink>
-          </List>
-
-          <div
-            className={classes.background}
-            style={{ backgroundImage: `url(${image})` }}
-          />
-        </Drawer>
-      </BrowserRouter>
+      <MySidebar
+        image={image}
+        logo={logo}
+        logoText={logoText}
+        route={route}
+      />
     )
   }
 }
 
-App = withStyles(sidebarStyle)(App)
-
-
 
 ReactDOM.render(
+  <BrowserRouter>
     <App
       name="test_app"
       image={image}
       logo={logo}
       logoText={'Cost Pro'}
       route={route}
-    />,
-    document.getElementById('test_field')
+    />
+  </BrowserRouter>,
+  document.getElementById('test_field')
 );
